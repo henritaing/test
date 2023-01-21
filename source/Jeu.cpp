c22ec400e096c73fa02bdd4dd1938cfca0373190
henritaing <95174330+henritaing@users.noreply.github.com>
2023-01-21 15:39:34 +0100

    Create Jeu.cpp

---
+++ source/Jeu.cpp
@@ -0,0 +1,38 @@
+
+Jeu::Jeu(){
+    tourActuel = 0;
+    gameOver = false;
+    joueurActuel = 0;
+    setup();
+}
+
+Jeu::~Jeu(){}
+
+void Jeu::setup(){
+    //Initialize board
+    //Initialize rules
+}
+
+void Jeu::jouerTour(){
+    tourActuel++;
+    //Get player's action
+    //Move the token
+    //Check if the game is over
+    if(estFini()){
+        gameOver = true;
+        afficherResultats();
+        return;
+    }
+    joueurActuel = (joueurActuel + 1) % 4;
+}
+
+bool Game::estFini(){
+    //Check if a player has run out of properties
+    //Check if a player has reached a certain amount of money
+    return false;
+}
+
+void Game::afficherResultats(){
+    //Print winner
+    //Print final state of the game
+}
